d8a2d6638ef546b235c60540b4df04255f5ff14e
MarvinOuma <167431203+MarvinOuma@users.noreply.github.com>
2024-10-18 11:06:54 +0300

    Add files via upload

---
+++ Accommodations data.sql
@@ -0,0 +1,10 @@
+Insert into Accommodations values(1, 'Sunrise Inn', 'Hotel', '101', '1234567890', '4', '200', '1');
+Insert into Accommodations values(2, 'Blue Skies', 'Resort', '102', '1234567891', '5', '500', '2');
+Insert into Accommodations values(3, 'Ocean Breeze', 'Hostel', '103', '1234567892', '3', '100', '1');
+Insert into Accommodations values(4, 'Mountain View', 'Hotel', '104', '1234567893', '4', '250', '3');
+Insert into Accommodations values(5, 'City Lights', 'Apartment', '105', '1234567894', '5', '400', '2');
+Insert into Accommodations values(6, 'Forest Retreat', 'Cottage', '106', '1234567895', '4', '350', '3');
+Insert into Accommodations values(7, 'Beach House', 'Resort', '107', '1234567896', '5', '600', '1');
+Insert into Accommodations values(8, 'Riverfront', 'Hostel', '108', '1234567897', '3', '120', '4');
+Insert into Accommodations values(9, 'Desert Oasis', 'Hotel', '109', '1234567898', '4', '220', '5');
+Insert into Accommodations values(10, 'Hilltop Haven', 'Cottage', '110', '1234567899', '5', '450', '6');
