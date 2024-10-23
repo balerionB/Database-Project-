60e8d54ff2f63e9085faa820ee8bd6338b116516
Squiffy1 <167431344+Squiffy1@users.noreply.github.com>
2024-10-23 19:24:10 +0300

    Add files via upload

---
+++ transportation.sql
@@ -0,0 +1,10 @@
+
+insert into Transportation values('2', 'Bus','Mascardi', 'Kisumu', 'Eldoret','7:10','13:40');
+insert into Transportation values('3', 'Train','Kenya Railways', 'Nairobi', 'Mombasa','10:00','14:20');
+insert into Transportation values('4', 'Ship','KSAA', 'Spain', 'Morocco','00:00','20:00');
+insert into Transportation values('5', 'flight','Kenya Airways', 'Nairobi', 'Ethopia','5:00','17:00');
+insert into Transportation values('6', 'Train','Kenya Railways', 'Nakuru', 'Uganda','04:30','21:00');
+insert into Transportation values('7', 'Ship','KSAA', 'Mombasa', 'Mogadishu','9:00','16:00');
+insert into Transportation values('8', 'Flight','Kenya Airways', 'Nairobi', 'Eldoret','9:00','9:30');
+insert into Transportation values('9', 'Car','Thrifty', 'Nyeri', 'Muranga','12:24','16:30');
+insert into Transportation values('10', 'Bus','Mascardi', 'Nakuru', 'Tanzania','00:00','23:38');
