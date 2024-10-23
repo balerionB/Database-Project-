0bb513247c11bf88fbd2294d5cc3ce9a87a486c7
Squiffy1 <167431344+Squiffy1@users.noreply.github.com>
2024-10-23 09:48:51 +0300

    Add files via upload

---
+++ user data.sql
@@ -0,0 +1,10 @@
+insert into Users values('20','Benja','benja20@gmail.com','0720263187','7546','Zoo');
+insert into Users values('21','Stacey','stacey349@gmail.com','0130263187','1246','Mountain');
+insert into Users values('22','Ian','ian291@gmail.com','0720263980','7096','Beach');
+insert into Users values('23','Lysa','lysa354@gmail.com','0714363187','7203','Hills');
+insert into Users values('24','Ben','ben890@gmail.com','0720673187','5646','Islands');
+insert into Users values('25','Zawadi','zawadi002@gmail.com','0740963187','9846','Forests');
+insert into Users values('26','Tom','tom786@gmail.com','0790263187','8046','Towns');
+insert into Users values('27','Sandra','sandra450@gmail.com','0720267637','6546','Cultural places');
+insert into Users values('28','Gift','gift7@gmail.com','0720256737','0576','Beach');
+insert into Users values('29','Jane','jane901@gmail.com','0720862187','4346','Mountain');
