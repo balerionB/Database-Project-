0f7b19b981e863db726e99005cd3436fdcffd43a
bravintoo <167431403+bravin23409@users.noreply.github.com>
2024-11-13 10:21:09 +0300

    Add files via upload

---
+++ activities table.sql
@@ -0,0 +1,11 @@
+INSERT INTO Activities (ActivityID, name, description, duration, price, destinationID) VALUES
+(1, 'Hiking', 'Mountain trail hike', '2 hours', 50, 101),
+(2, 'Snorkeling', 'Coral reef snorkeling', '3 hours', 100, 102),
+(3, 'City Tour', 'Guided city tour', '4 hours', 75, 103),
+(4, 'Wine Tasting', 'Vineyard wine tasting', '1 hour', 40, 104),
+(5, 'Skydiving', 'Tandem skydiving', '30 minutes', 200, 105),
+(6, 'Swimming', 'Pool swimming session', '1 hour', 30, 106),
+(7, 'Cycling', 'Mountain biking', '2 hours', 60, 107),
+(8, 'Football', 'Team football match', '1.5 hours', 20, 108),
+(9, 'Team Building', 'Corporate team building', '3 hours', 150, 109),
+(10, 'Kayaking', 'River kayaking adventure', '2 hours', 80, 110);
